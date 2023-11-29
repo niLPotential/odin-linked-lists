97bc1ff7040cd5198f8ba356479d3e567685e0e0
niLPotential <niLPotential@proton.me>
2023-11-29 22:00:08 +0900

    Add createNode factory

--- main.ts
+++ main.ts
@@ -6,3 +6,26 @@ export function add(a: number, b: number): number {
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
 }
+
+function createNode<T>() {
+  let value = null as T;
+  let nextNode = null as T;
+
+  function getValue() {
+    return value;
+  }
+
+  function setValue(newValue: T) {
+    value = newValue;
+  }
+
+  function getNextNode() {
+    return nextNode;
+  }
+
+  function setNextNode(newNextNode: T) {
+    nextNode = newNextNode;
+  }
+
+  return { getValue, setValue, getNextNode, setNextNode };
+}
