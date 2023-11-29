2ebf2bbad395a4b9eff797c4e2da751faadfdbf3
niLPotential <niLPotential@proton.me>
2023-11-29 22:27:02 +0900

    Add append, size, head, tail

--- main.js
+++ main.js
@@ -1,5 +1,45 @@
 export function createLinkedList() {
   let headNode = null;
+
+  function append(value) {
+    const newNode = createNode();
+    newNode.setValue(value);
+    if (headNode === null) {
+      headNode = newNode;
+    } else {
+      this.tail().nextNode = newNode;
+    }
+  }
+
+  function size() {
+    if (headNode === null) {
+      return 0;
+    }
+    let result = 1;
+    let node = headNode;
+    while (node.getNextNode() !== null) {
+      node = node.getNextNode();
+      result++;
+    }
+    return result;
+  }
+
+  function head() {
+    return headNode;
+  }
+
+  function tail() {
+    if (headNode === null) {
+      return null;
+    }
+    let node = headNode;
+    while (node.nextNode !== null) {
+      node = node.nextNode;
+    }
+    return node;
+  }
+
+  return { append, size, head, tail };
 }
 
 function createNode() {
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,9 @@
-// import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
 import { createLinkedList } from "./main.js";
 
-// Deno.test(function addTest() {
-//   assertEquals(add(2, 3), 5);
-// });
+Deno.test(function linkedListTest() {
+  const linkedList = createLinkedList();
+  linkedList.append("first");
+  assertEquals(linkedList.head().getValue(), "first");
+  assertEquals(linkedList.size(), 1);
+});
